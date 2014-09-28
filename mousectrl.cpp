98aa95ae1b916984905a7dca39a45d865b942f50
Rajat Agarwal <rajat503@gmail.com>
2014-09-28 11:56:42 +0530

    removed commented code

--- mousectrl.cpp
+++ mousectrl.cpp
@@ -22,77 +22,6 @@ void mouseMove(int x, int y)
 
     XCloseDisplay(displayMain);
 }
-/*
-void segmentation(Mat orig_img)
-{
-	int rows=orig_img.rows;
-	int cols=orig_img.cols;
-	Mat img_gray;
-	Mat img_output = Mat(rows, cols, CV_8UC3);
-	uchar* p;
-	uchar* q;
-	uchar* r;
-	int i, j;
-	int ib,ig,ir;
-	ib=orig_img.ptr<uchar>(0)[0];
-	ig=orig_img.ptr<uchar>(0)[1];
-	ir=orig_img.ptr<uchar>(0)[2];
-	for(i=0;i<orig_img.rows; i++)
-	{
-		p=orig_img.ptr<uchar>(i);
-		q=img_output.ptr<uchar>(i);
-		for(j=0;j<orig_img.cols*orig_img.channels();j+=orig_img.channels())
-		{
-			if((p[j]==ib && p[j+1]==ig && p[j+2]==ir) || (p[j]==255 && p[j+1]==255 && p[j+2]==255)) 
-			{
-				q[j]=0;
-				q[j+1]=0;
-				q[j+2]=0;
-			}
-			else
-			{
-				q[j]=p[j];
-				q[j+1]=p[j+1];
-				q[j+2]=p[j+2];	
-			}
-		}		
-	}
-	cvtColor(img_output,img_gray, COLOR_BGR2GRAY);
-	threshold(img_gray, img_gray, 0, 255, THRESH_BINARY);
-	Mat element = getStructuringElement(0,Size(2*3+1,2*3+1),Point(3,3));
-	for(i=0;i<3;i++)
-	{
-		erode(img_gray, img_gray, element);
-	}
-	for(i=0;i<3;i++)
-	{
-		dilate(img_gray, img_gray, element);
-	}
-	for(i=0;i<orig_img.rows; i++)
-	{
-		r=img_gray.ptr<uchar>(i);
-		p=orig_img.ptr<uchar>(i);
-		q=img_output.ptr<uchar>(i);
-		for(j=0;j<img_gray.cols;j++)
-		{
-			if(r[j]==255) 
-			{
-				q[j*orig_img.channels()]=p[j*orig_img.channels()];
-				q[j*orig_img.channels()+1]=p[j*orig_img.channels()+1];
-				q[j*orig_img.channels()+1]=p[j*orig_img.channels()+1];
-			}
-			else
-			{
-				q[j*orig_img.channels()]=0;
-				q[j*orig_img.channels()+1]=0;
-				q[j*orig_img.channels()+2]=0;	
-			}
-		}		
-	}
-	imshow("Output3",img_output); 
-	waitKey(0);
-	imwrite("Output3.jpg", img_output);
-} */
 
 int main()
 {
@@ -115,10 +44,6 @@ int main()
 		{
 			dilate(green, green, element);
 		}
-		/*for(i=0;i<3;i++)
-		{
-			erode(green, green, element);
-		}*/
 		Moments mu;
     	Point2f mc;
     	mu = moments(green);
